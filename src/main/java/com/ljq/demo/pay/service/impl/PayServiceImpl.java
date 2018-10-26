a424018ae0438e9ff7655e2c8ef7a1db54c37759
junqiang.lu <flying9001@gmail.com>
2018-10-26 10:01:05 +0800

    提高二方库依赖版本,避免二方库导致的系统漏洞

--- src/main/java/com/ljq/demo/pay/service/impl/PayServiceImpl.java
+++ src/main/java/com/ljq/demo/pay/service/impl/PayServiceImpl.java
@@ -13,7 +13,6 @@ import com.ljq.demo.pay.service.PayService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
